4af581340eb3a87c048bea322f7581aed50337f8
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-03-26 21:13:13 +0200

    Added DataContext implementation

---
+++ Shop/Data/Implementation/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop.Data
+{
+    public class DataContext : IDataContext
+    {
+        public DataContext() {
+            this.Users = new Dictionary<string, IUser>();
+        }
+
+        public Dictionary<string, IUser> Users { get; set; }
+    }
+}
