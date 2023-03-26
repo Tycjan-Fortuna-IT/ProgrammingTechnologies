aeba8ab4843ae7cc5a01f49c03016df64c59502c
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-03-26 21:13:05 +0200

    Added IDataRepository to API of the data layer

---
+++ Shop/Data/API/IDataRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Data
+{
+    public interface IDataRepository
+    {
+        void AddUser(IUser user);
+        IUser GetUser(string guid);
+        void UpdateUser(string guid, IUser user);
+        void DeleteUser(string guid);
+        Dictionary<string, IUser> GetAllUsers();
+        int GetUsersCount();
+    }
+}
