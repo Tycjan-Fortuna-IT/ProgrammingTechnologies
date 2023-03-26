82099921ee05efa2f4be574f9314433d5ebfdc92
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-03-26 21:13:29 +0200

    Added DataRepository implementation

---
+++ Shop/Data/Implementation/DataRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace Shop.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        public DataRepository(IDataContext context) {
+            this.context = context;
+        }
+
+        private IDataContext context;
+
+        public void AddUser(IUser User) {
+            if (!this.context.Users.ContainsKey(User.Guid)) {
+                this.context.Users.Add(User.Guid, User);
+            } else {
+                throw new Exception("This User already exists!");
+            }
+        }
+
+        public IUser GetUser(string guid) {
+            if (this.context.Users.ContainsKey(guid)) { 
+                return this.context.Users[guid]; 
+            } else {
+                throw new Exception("This User does not exist!");
+            }
+        }
+
+        public void UpdateUser(string guid, IUser User) {
+            if (this.context.Users.ContainsKey(guid))
+            {
+                this.context.Users[guid] = User;
+            } else {
+                throw new Exception("This User does not exist!");
+            }
+        }
+        
+        public void DeleteUser(string guid) {
+            if (this.context.Users.ContainsKey(guid))
+            {
+                this.context.Users.Remove(guid);
+            } else {
+                throw new Exception("This User does not exist!");
+            }
+        }
+
+        public Dictionary<string, IUser> GetAllUsers() {
+            return this.context.Users;
+        }
+        
+        public int GetUsersCount() {
+            return this.context.Users.Count();
+        }
+    }
+}
