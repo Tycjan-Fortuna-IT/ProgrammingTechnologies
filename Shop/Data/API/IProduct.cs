4d9b8929f0e35724a77ec28059d8d8e392678fd2
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-03-31 20:20:50 +0200

    Added IProduct and Game to Data layer

---
+++ Shop/Data/API/IProduct.cs
@@ -0,0 +1,23 @@
+﻿namespace Shop.Data
+{
+    public interface IProduct
+    {
+        string Guid { get; }
+
+        string Name { get; set; }
+
+        double Price { get; set; }
+
+        int Quantity { get; set; }
+
+        IProducer Producer { get; set; }
+
+        int Genre { get; set; }
+
+        DateTime RelaseDate { get; set; }
+
+        int PEGI { get; set; }
+
+        string GetGenre();
+    }
+}
---
+++ Shop/Data/Implementation/Game.cs
@@ -0,0 +1,38 @@
+﻿namespace Shop.Data
+{
+    public class Game : IProduct
+    {
+        public Game(string? Guid, string Name, double Price, int Quantity, IProducer Producer,
+            int Genre, DateTime RelaseDate, int PEGI)
+        {
+            this.Guid = Guid ?? System.Guid.NewGuid().ToString();
+            this.Name = Name;
+            this.Price = Price;
+            this.Quantity = Quantity;
+            this.Producer = Producer;
+            this.Genre = Genre;
+            this.RelaseDate = RelaseDate;
+            this.PEGI = PEGI;
+        }
+
+        public string Guid { get; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public IProducer Producer { get; set; }
+
+        public int Genre { get; set; }
+
+        public DateTime RelaseDate { get; set; }
+
+        public int PEGI { get; set; }
+
+        public string GetGenre() {
+            return Enum.GetName(typeof(GenreEnum), this.Genre);
+        }
+    }
+}
