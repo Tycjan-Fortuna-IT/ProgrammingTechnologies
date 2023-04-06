717f3514af57b9df0ce17ddd5354d5c180983ecc
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-04-06 17:23:55 +0200

    IDataContext and IDataRepository changes, more event methods

--- Shop/Data/API/IDataContext.cs
+++ Shop/Data/API/IDataContext.cs
@@ -4,14 +4,6 @@ namespace Shop.Data
 {
     public abstract class IDataContext
     {
-        Dictionary<string, IUser> Users = new Dictionary<string, IUser>();
-
-        Dictionary<string, IProduct> Products = new Dictionary<string, IProduct>();
-
-        Dictionary<string, IEvent> Events = new Dictionary<string, IEvent>();
-        
-        Dictionary<string, IState> States = new Dictionary<string, IState>();
-
         public Dictionary<Type, IDictionary<string, IElement>> Elements = new Dictionary<Type, IDictionary<string, IElement>>
         {
             { typeof(IUser), new Dictionary<string, IElement>() },
--- Shop/Data/API/IDataRepository.cs
+++ Shop/Data/API/IDataRepository.cs
@@ -3,10 +3,21 @@
     public interface IDataRepository
     {
         void Add<T>(T element) where T : IElement;
+
         T Get<T>(string Guid) where T : IElement;
+
         void Update<T>(string Guid, T element) where T : IElement;
+
         void Delete<T>(string Guid) where T : IElement;
+
         Dictionary<string, T> GetAll<T>() where T : IElement;
+
         int GetCount<T>() where T : IElement;
+
+        IEvent GetLastProductEvent(string ProductGuid);
+
+        List<IEvent> GetProductEventHistory(string ProductGuid);
+
+        IState GetProductState(string ProductGuid);
     }
 }
