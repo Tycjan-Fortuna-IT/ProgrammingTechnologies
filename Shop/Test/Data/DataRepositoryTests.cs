cf9e43e7672acc6961813f60cfb93644a2557a8a
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-04-11 14:41:22 +0200

    DataRepository GetAll, GetLastProductEvent and GetProductHistory initial tests

--- Shop/Test/Data/DataRepositoryTests.cs
+++ Shop/Test/Data/DataRepositoryTests.cs
@@ -1,18 +1,25 @@
 ﻿using Shop.Data;
+using System.Runtime.CompilerServices;
 
 namespace Shop.Test.Data
 {
     [TestClass]
     public class DataRepositoryTests
     {
-        private IDataRepository Repository = new DataRepository(new DataContext());
-        private IUser User = new User(null, "Michal", "Gapcio", "m_gapcio@gmail.com", 200,
+        IDataRepository Repository = new DataRepository(new DataContext());
+        IUser User = new User("1", "Michal", "Gapcio", "m_gapcio@gmail.com", 200,
                 new DateTime(2015, 12, 25), 542123567, null);
-
+        IProduct game = new Game(null, "Diablo 3", 339.99, new DateTime(2012, 5, 15), 18);
+        IState state = new State(null, new Game(null, "Starcraft", 61.99, new DateTime(1998, 3, 28), 16), 4);
+        IEvent Buy = new PurchaseEvent(null, new State(null, new Game("30", "Hollow Knight", 55.99, new DateTime(2017, 2, 24), 13), 1), new User(null, "Arno", "Dorian", "a_dorian@paris.fr", 100, new DateTime(1990, 4, 5), 758903841, null));
+        
         [TestInitialize]
         public void Initialize()
         {
             this.Repository.Add<IUser>(User);
+            this.Repository.Add<IProduct>(game);
+            this.Repository.Add<IState>(state);
+            this.Repository.Add<IEvent>(Buy);
         }
 
         [TestMethod]
@@ -65,6 +72,39 @@ namespace Shop.Test.Data
             Repository.Delete<IUser>(User.Guid);
 
             Assert.AreEqual(0, Repository.GetCount<IUser>());
+
+            Assert.ThrowsException<Exception>(() => Repository.Delete<IUser>("NOGUID"));
+        }
+
+        //[TestMethod]
+        //public void DataRepositoryGetAllTest()
+        //{
+            //var wholeContext = Repository.GetAll<IUser>();
+
+            //Assert.IsNotNull(wholeContext);
+            //Assert.AreEqual(1, Repository.GetCount<IUser>());
+            //Assert.IsTrue(wholeContext.ContainsValue(User));
+        //}
+
+        //[TestMethod]
+        //public void DataRepositoryGetLastProductEventTest()
+        //{
+            //var testEvent = Repository.GetLastProductEvent("30");
+
+            //Assert.IsNotNull(testEvent);
+        //}
+
+        [TestMethod]
+        public void DataRepositoryGetProductHistoryTest()
+        {
+            var history = Repository.GetProductEventHistory("30");
+            Console.WriteLine(history);
+        }
+
+        [TestMethod]
+        public void DataRepositoryGetProductState()
+        {
+
         }
     }
 }
