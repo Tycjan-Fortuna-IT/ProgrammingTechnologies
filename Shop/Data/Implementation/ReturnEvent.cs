0e32e05a8daa0a110a8e1bad89717c73e25c6055
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-04-15 09:12:41 +0200

    Changed to old and commented out my proposition

--- Shop/Data/Implementation/ReturnEvent.cs
+++ Shop/Data/Implementation/ReturnEvent.cs
@@ -2,7 +2,8 @@ namespace Shop.Data
 {
     public class ReturnEvent : IEvent
     {
-        public ReturnEvent(string? Guid, IState State, IUser User) {
+        public ReturnEvent(string? Guid, IState State, IUser User)
+        {
             this.Guid = Guid ?? System.Guid.NewGuid().ToString();
             this.State = State;
             this.User = User;
@@ -21,19 +22,26 @@ namespace Shop.Data
 
         public void Action()
         {
-            if (!this.User.ProductLibrary.ContainsValue(this.State.Product))
+            if (!this.User.ProductLibrary.ContainsKey(this.State.Product.Guid))
                 throw new Exception("You do not have this Product!");
 
             this.State.ProductQuantity++;
             this.User.Balance += this.State.Product.Price;
-            foreach (KeyValuePair<string, IProduct> item in this.User.ProductLibrary) 
-                if (item.Value == this.State.Product)
-                {
-                    this.User.ProductLibrary.Remove(item.Key);
-                    break;
-                }
-            
-            //this.User.ProductLibrary.Remove(this.State.Product.Guid);
+            this.User.ProductLibrary.Remove(this.State.Product.Guid);
         }
+
+        //public void Action()
+        //{
+        //if (!this.User.ProductLibrary.ContainsKey(this.State.Product.Guid))
+        //throw new Exception("You do not have this Product!");
+        //this.State.ProductQuantity++;
+        //this.User.Balance += this.State.Product.Price;
+        //foreach (KeyValuePair<string, IProduct> item in this.User.ProductLibrary) 
+        //if (item.Value == this.State.Product)
+        //{
+        //this.User.ProductLibrary.Remove(item.Key);
+        //break;
+        //}
+        //}
     }
 }
