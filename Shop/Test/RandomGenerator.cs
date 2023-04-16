1e5a4105ac239a94916bf2b7f7b0e24019f6afbe
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-04-16 19:17:13 +0200

    Added ReturnEvent tests

--- Shop/Test/RandomGenerator.cs
+++ Shop/Test/RandomGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using Data.API;
 using Data.Implementation;
+using System.Reflection.Metadata;
 
 namespace Shop.Test
 {
@@ -7,19 +8,56 @@ namespace Shop.Test
     {
         public void Generate(IDataContext context) 
         {
-            for (int i = 0; i < 20; i++) 
+            for (int i = 0; i < 20; i++)
             {
+                User user = new User(null, RandomString(10), RandomString(10), RandomEmail(),
+                    (RandomNumber<double>(4)), RandomDate(), RandomNumber<int>(9), null);
+                context.Users.Add(user);
+
                 Game game = new Game(null, RandomString(7), RandomNumber<double>(4), RandomDate(), RandomPEGI());
                 context.Products.Add(game);
 
-                User user = new User(null, RandomString(10), RandomString(10), RandomEmail(),
-                    (RandomNumber<double>(4) + game.Price), RandomDateOfBirth(game.PEGI), RandomNumber<int>(9), null);
-                context.Users.Add(user); 
-
                 State state = new State(null, game, RandomNumber<int>(2));
                 context.States.Add(state);
+            }
 
-                PurchaseEvent buyEvent = new PurchaseEvent(null, state, user);
+            Random random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {                
+                User user = (User)context.Users[i];
+
+                List<IState> availableGamesStates = context.States.FindAll(element => (element.Product.Price < user.Balance && 
+                                                    (((Game)element.Product).PEGI * 365) < (DateTime.Today - user.DateOfBirth).Days) &&
+                                                    !user.ProductLibrary.ContainsKey(element.Product.Guid) &&
+                                                    (element.ProductQuantity > 0));
+
+                if (availableGamesStates is not null && availableGamesStates.Count > 0)
+                    switch (availableGamesStates.Count)
+                    {
+                        case 1:
+                            PurchaseEvent eventPurchase0 = new PurchaseEvent(null, (State)availableGamesStates[0], user);
+                            context.Events.Add(eventPurchase0);
+                            break;
+                        default:
+                            State randomGameToPurchaseState = (State)availableGamesStates[random.Next(0, availableGamesStates.Count - 1)];
+                            PurchaseEvent eventPurchase1 = new PurchaseEvent(null, randomGameToPurchaseState, user);
+                            context.Events.Add(eventPurchase1);
+                            break;
+                    }
+            }
+
+            for (int i = 0; i < random.Next(5, 10); i++)
+            {
+                User randomUser = (User)context.Users[random.Next(context.Users.Count - 1)];
+
+                if (randomUser.ProductLibrary.Count > 0)
+                {
+                    Game purchasedGame = (Game)randomUser.ProductLibrary.First().Value;
+                    State purchasedGameState = (State)context.States.Find(element => element.Product.Guid == purchasedGame.Guid);
+                    ReturnEvent eventReturn = new ReturnEvent(null, purchasedGameState, randomUser);
+                    context.Events.Add(eventReturn);
+                }
             }
         }
 
@@ -97,26 +135,9 @@ namespace Shop.Test
         {
             Random random = new Random();
 
-            List<int> pegiRange = new List<int>() { 3, 7, 12, 16, 18};
+            List<int> pegiRange = new List<int>() { 3, 7, 12, 16, 18 };
 
             return random.Next(pegiRange.Count);
         }
-
-        public static DateTime RandomDateOfBirth(int pegi)
-        {
-            Random random = new Random();
-
-            DateTime date = new DateTime(1900, 1, 1);
-
-            int range = (DateTime.Today - date).Days;
-
-            int age = pegi * 365;
-
-            return date.AddDays(random.Next(age, range))
-                .AddHours(random.Next(24))
-                .AddMinutes(random.Next(60))
-                .AddSeconds(random.Next(60));
-        }
-
     }
 }
