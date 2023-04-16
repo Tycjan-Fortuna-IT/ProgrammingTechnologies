a2386df787dbdbb441a612696ee9e64916b39996
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-04-16 13:43:14 +0200

    Random Game, State and PurchaseEvent added to RandomGenerator

--- Shop/Test/RandomGenerator.cs
+++ Shop/Test/RandomGenerator.cs
@@ -5,48 +5,63 @@ namespace Shop.Test
 {
     public class RandomGenerator : IGenerator
     {
-        public void Generate(IDataContext Context) {
-            for (int i = 0; i < 20; i++) {
-                User user = new User(null, RandomString(10), RandomString(10), RandomEmail(),
-                    RandomNumber<double>(4), RandomDate(), RandomNumber<int>(9), null);
+        public void Generate(IDataContext context) 
+        {
+            for (int i = 0; i < 20; i++) 
+            {
+                Game game = new Game(null, RandomString(7), RandomNumber<double>(4), RandomDate(), RandomPEGI());
+                context.Products.Add(game);
 
-                Context.Users.Add(user);
+                User user = new User(null, RandomString(10), RandomString(10), RandomEmail(),
+                    (RandomNumber<double>(4) + game.Price), RandomDateOfBirth(game.PEGI), RandomNumber<int>(9), null);
+                context.Users.Add(user); 
+
+                State state = new State(null, game, RandomNumber<int>(2));
+                context.States.Add(state);
+
+                PurchaseEvent buyEvent = new PurchaseEvent(null, state, user);
             }
         }
 
-        public static string RandomString(int length) {
+        public static string RandomString(int length) 
+        {
             var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 
             var randomText = new char[length];
 
             Random random = new Random();
 
-            for (int i = 0; i < length; i++) {
+            for (int i = 0; i < length; i++) 
+            {
                 randomText[i] = chars[random.Next(chars.Length)];
             }
 
             return new string(randomText);
         }
 
-        public static string RandomStringWithNumber(int length) {
+        public static string RandomStringWithNumber(int length) 
+        {
             var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
             var randomText = new char[length];
 
             Random random = new Random();
 
-            for (int i = 0; i < length; i++) {
+            for (int i = 0; i < length; i++) 
+            {
                 randomText[i] = chars[random.Next(chars.Length)];
             }
 
             return new string(randomText);
         }
 
-        public static string RandomEmail() {
+        public static string RandomEmail() 
+        {
             return string.Format("{0}@{1}.com", RandomStringWithNumber(10), RandomString(5));
         }
 
-        public static T RandomNumber<T>(int length) where T : struct, IComparable {
+        public static T RandomNumber<T>(int length) where T : struct, IComparable 
+        {
             if (length <= 0)
                 throw new ArgumentException("Number of digits must be positive.");
 
@@ -64,7 +79,8 @@ namespace Shop.Test
             return randomNumber;
         }
 
-        public static DateTime RandomDate() {
+        public static DateTime RandomDate() 
+        {
             Random random = new Random();
 
             DateTime date = new DateTime(1900, 1, 1);
@@ -72,6 +88,31 @@ namespace Shop.Test
             int range = (DateTime.Today - date).Days;
 
             return date.AddDays(random.Next(range))
+                .AddHours(random.Next(24))
+                .AddMinutes(random.Next(60))
+                .AddSeconds(random.Next(60));
+        }
+
+        public static int RandomPEGI()
+        {
+            Random random = new Random();
+
+            List<int> pegiRange = new List<int>() { 3, 7, 12, 16, 18};
+
+            return random.Next(pegiRange.Count);
+        }
+
+        public static DateTime RandomDateOfBirth(int pegi)
+        {
+            Random random = new Random();
+
+            DateTime date = new DateTime(1900, 1, 1);
+
+            int range = (DateTime.Today - date).Days;
+
+            int age = pegi * 365;
+
+            return date.AddDays(random.Next(age, range))
                 .AddHours(random.Next(24))
                 .AddMinutes(random.Next(60))
                 .AddSeconds(random.Next(60));
