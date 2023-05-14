54e00be0d9d92bfae157681e9b39da22caf7f8e9
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-05-14 16:35:26 +0200

    ShopTab View added

---
+++ Shop/Presentation/View/ShopTab.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Presentation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ShopTab : Window
+    {
+        public ShopTab()
+        {
+            InitializeComponent();
+        }
+
+        private void searchDatabaseGames(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void purchaseGame(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
