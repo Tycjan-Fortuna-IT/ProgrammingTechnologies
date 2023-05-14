ae1d795c657bda07ca6e386fa0b1e5de6621488f
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-05-14 17:39:29 +0200

    UpdateViewModel added

---
+++ Shop/Presentation/ViewModel/UpdateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Presentation.ViewModel
+{
+    class UpdateViewModel : ICommand
+    {
+        private IViewModel _viewModel;
+
+        public event EventHandler CanExecuteChanged;
+
+        public UpdateViewModel(IViewModel viewModel) 
+        {
+            this._viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            this._viewModel.SelectedViewModel = new ShopTabViewModel();
+        }
+    }
+}
