ec55ffbbd566dd4bb407e9000760ad3b9ba2528b
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-05-14 16:35:01 +0200

    IViewModel interface added

---
+++ Shop/Presentation/ViewModel/IViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Presentation.ViewModel
+{
+    public class IViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
