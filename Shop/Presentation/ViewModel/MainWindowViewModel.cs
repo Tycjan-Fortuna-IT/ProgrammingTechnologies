7141aa9640250d2139c8c49d31857e373e2a35ac
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-05-14 17:45:05 +0200

    MainWindow and intial ShopTab View Model added

--- Shop/Presentation/ViewModel/MainWindowViewModel.cs
+++ Shop/Presentation/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using System.ComponentModel;
+using System.Windows.Input;
 
 namespace Presentation.ViewModel
 {
     public partial class MainWindowViewModel : IViewModel
     {
-        MainWindowViewModel()
-        {
+        private IViewModel _selectedViewModel;
 
+        public ICommand StartAppCommand;
+
+        public ICommand ExitAppCommand;
+        
+        public MainWindowViewModel()
+        {
+            this.StartAppCommand = new UpdateViewModel(this);
+        }
+
+        public IViewModel SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        public void switchView()
+        {
+            _selectedViewModel = new ShopTabViewModel();
+            //_selectedViewModel.Show();
         }
     }
 }
