678003c48cc1fd16db6f37f8fe620278c7809173
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-05-15 20:22:10 +0200

    Added services for the States

---
+++ Shop/Service/API/IStateCRUD.cs
@@ -0,0 +1,18 @@
+﻿using Data.API;
+
+namespace Service.API;
+
+public interface IStateCRUD
+{
+    Task AddStateAsync(int id, int productId, int productQuantity);
+
+    Task<IStateDTO> GetStateAsync(int id);
+
+    Task UpdateStateAsync(int id, int productId, int productQuantity);
+
+    Task DeleteStateAsync(int id);
+
+    Task<Dictionary<int, IStateDTO>> GetAllStatesAsync();
+
+    Task<int> GetStatesCountAsync();
+}
