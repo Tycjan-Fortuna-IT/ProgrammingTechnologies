0f5fc27164a01a4fd5cfd4bacf578c59a709f625
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-05-15 20:22:01 +0200

    Added services for the Products

---
+++ Shop/Service/API/IProductCRUD.cs
@@ -0,0 +1,18 @@
+﻿using Data.API;
+
+namespace Service.API;
+
+public interface IProductCRUD
+{
+    Task AddProductAsync(int id, string name, double price, int pegi);
+
+    Task<IProductDTO> GetProductAsync(int id);
+
+    Task UpdateProductAsync(int id, string name, double price, int pegi);
+
+    Task DeleteProductAsync(int id);
+
+    Task<Dictionary<int, IProductDTO>> GetAllProductsAsync();
+
+    Task<int> GetProductsCountAsync();
+}
