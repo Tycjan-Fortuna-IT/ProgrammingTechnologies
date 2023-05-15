681dcd0ad79ea7e86a5e97b72ddb943f1f6242d5
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-05-15 20:22:16 +0200

    Added services for the Users

---
+++ Shop/Service/API/IUserCRUD.cs
@@ -0,0 +1,16 @@
+﻿namespace Service.API;
+
+public interface IUserCRUD
+{
+    Task AddUserAsync(int id, string nickname, string email, double balance, DateTime dateOfBirth);
+
+    Task<IUserDTO> GetUserAsync(int id);
+
+    Task UpdateUserAsync(int id, string nickname, string email, double balance, DateTime dateOfBirth);
+
+    Task DeleteUserAsync(int id);
+
+    Task<Dictionary<int, IUserDTO>> GetAllUsersAsync();
+
+    Task<int> GetUsersCountAsync();
+}
