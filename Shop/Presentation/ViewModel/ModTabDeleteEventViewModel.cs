7954fc25dd0690985939a0b938cc5bc30d7fb73e
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-05-16 12:41:22 +0200

    ModTabDeleteEvent View and ViewModel added

---
+++ Shop/Presentation/ViewModel/ModTabDeleteEventViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Presentation.ViewModel
+{
+    public partial class ModTabDeleteEventViewModel : IViewModel
+    {
+        public ModTabDeleteEventViewModel()
+        {
+
+        }
+
+
+    }
+}
