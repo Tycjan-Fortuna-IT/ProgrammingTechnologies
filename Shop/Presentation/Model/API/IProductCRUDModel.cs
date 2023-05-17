84eae7ece13acad020f9b709a2ac135a9af9bf97
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-05-17 03:50:37 +0200

    API for Models, still fixing

---
+++ Shop/Presentation/Model/API/IProductCRUDModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.API;
+using Service.API;
+using Presentation.Model.Implementation;
+
+namespace Presentation.Model.API
+{
+    public interface IProductCRUDModel
+    {
+        static IProductCRUDModel CreateProductCRUD(IDataRepository? dataRepository)
+        {
+            return new ProductCRUDModel(dataRepository ?? IDataRepository.CreateDatabase());
+        }
+
+        Task AddProductAsync(int id, string name, double price, int pegi);
+
+        Task<IProductDTOModel> GetProductAsync(int id);
+
+        Task UpdateProductAsync(int id, string name, double price, int pegi);
+
+        Task DeleteProductAsync(int id);
+
+        Task<Dictionary<int, IProductDTOModel>> GetAllProductsAsync();
+
+        Task<int> GetProductsCountAsync();
+    }
+}
---
+++ Shop/Presentation/Model/API/IUserCRUDModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.API;
+using Service.API;
+using Presentation.Model.Implementation;
+
+namespace Presentation.Model.API
+{
+    public interface IUserCRUDModel
+    {
+        static IUserCRUDModel CreateUserCRUD(IDataRepository? dataRepository)
+        {
+            return new UserCRUDModel(dataRepository ?? IDataRepository.CreateDatabase());
+        }
+
+        Task AddUserAsync(int id, string nickname, string email, double balance, DateTime dateOfBirth);
+
+        Task<IUserDTOModel> GetUserAsync(int id);
+
+        Task UpdateUserAsync(int id, string nickname, string email, double balance, DateTime dateOfBirth);
+
+        Task DeleteUserAsync(int id);
+
+        Task<Dictionary<int, IUserDTOModel>> GetAllUsersAsync();
+
+        Task<int> GetUsersCountAsync();
+    }
+}
