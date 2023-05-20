ac71e1da3387d4289fb6b74a760adb0cdb9057b6
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-05-20 12:55:50 +0200

    Event Detail viewmodel added

---
+++ Shop/Presentation/ViewModel/Event/EventDetailViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Service.API;
+using System;
+using System.Windows.Input;
+
+namespace Presentation.ViewModel;
+
+internal class EventDetailViewModel : IViewModel
+{
+    public ICommand UpdateEvent { get; set; }
+
+    private IEventCRUD _service { get; set; }
+
+    private int _id;
+
+    public int Id
+    {
+        get => _id;
+        set
+        {
+            _id = value;
+            OnPropertyChanged(nameof(Id));
+        }
+    }
+
+    private int _stateId;
+
+    public int StateId
+    {
+        get => _stateId;
+        set
+        {
+            _stateId = value;
+            OnPropertyChanged(nameof(StateId));
+        }
+    }
+
+    private int _userId;
+
+    public int UserId
+    {
+        get => _userId;
+        set
+        {
+            _userId = value;
+            OnPropertyChanged(nameof(UserId));
+        }
+    }
+
+    private DateTime _occurrenceDate;
+
+    public DateTime OccurrenceDate
+    {
+        get => _occurrenceDate;
+        set
+        {
+            _occurrenceDate = value;
+            OnPropertyChanged(nameof(OccurrenceDate));
+        }
+    }
+
+    public EventDetailViewModel()
+    {
+
+    }
+
+    public EventDetailViewModel(int id, int stateId, int userId, DateTime occurrenceDate)
+    {
+        this.Id = id;
+        this.StateId = stateId;
+        this.UserId = userId;
+        this.OccurrenceDate = occurrenceDate;
+    }
+
+    //private void Update()
+    //{
+    //    Task.Run(() =>
+    //    {
+    //        this._service.UpdateEventAsync(this.Id, this.StateId);
+    //    });
+    //}
+
+    //private bool CanUpdate()
+    //{
+    //    return !(
+    //        string.IsNullOrWhiteSpace(this.ProductQuantity.ToString()) ||
+    //        this.ProductQuantity < 0
+    //    );
+    //}
+}
