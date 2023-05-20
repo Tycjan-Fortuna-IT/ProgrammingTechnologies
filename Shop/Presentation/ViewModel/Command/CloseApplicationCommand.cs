37a8ff36fbf3cd4971c5ea01cc1f96fcf5deee5b
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-05-20 12:55:31 +0200

    onclick and switch view commands rework

--- Shop/Presentation/ViewModel/Command/CloseApplicationCommand.cs
+++ Shop/Presentation/ViewModel/Command/CloseApplicationCommand.cs
@@ -7,7 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace Presentation.ViewModel.Command
+namespace Presentation.ViewModel
 {
     public class CloseApplicationCommand : ICommand
     {
