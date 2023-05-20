91e54f83f5ba04821e438a5a7fdeaf4084328437
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-05-20 17:45:31 +0200

    Product's model added (mvvm)

---
+++ Shop/Presentation/Model/API/IProductModelOperation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Presentation.Model.Implementation;
+
+namespace Presentation.Model.API;
+
+public interface IProductModelOperation
+{
+    static IProductModelOperation CreateModelOperation()
+    {
+        return new ProductModelOperation();
+    }
+
+    Task AddAsync(int id, string name, double price, int pegi);
+
+    Task<IProductModel> GetAsync(int id);
+
+    Task UpdateAsync(int id, string name, double price, int pegi);
+
+    Task DeleteAsync(int id);
+
+    Task<Dictionary<int, IProductModel>> GetAllAsync();
+
+    Task<int> GetCountAsync();
+}
