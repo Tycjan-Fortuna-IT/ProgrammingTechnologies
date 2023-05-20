819401b7f9fa490c19fce71ae7eb2287185feba0
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-05-20 17:45:02 +0200

    Event's model added (mvvm)

---
+++ Shop/Presentation/Model/API/IEventModelOperation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Presentation.Model.Implementation;
+
+namespace Presentation.Model.API;
+
+public interface IEventModelOperation
+{
+    static IEventModelOperation CreateModelOperation()
+    {
+        return new EventModelOperation();
+    }
+
+    Task AddAsync(int id, int stateId, int userId, string type, int quantity = 0);
+
+    Task<IEventModel> GetAsync(int id, string type);
+
+    Task UpdateAsync(int id, int stateId, int userId, DateTime occurrenceDate, string type, int? quantity);
+
+    Task DeleteAsync(int id);
+
+    Task<Dictionary<int, IEventModel>> GetAllAsync();
+
+    Task<int> GetCountAsync();
+}
