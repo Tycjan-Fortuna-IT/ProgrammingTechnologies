cf16e45a0cdee01841b752cdac7e7ed43f174240
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-05-20 17:45:42 +0200

    State's model added (mvvm)

---
+++ Shop/Presentation/Model/API/IStateModelOperation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Presentation.Model.Implementation;
+
+namespace Presentation.Model.API;
+
+public interface IStateModelOperation
+{
+    static IStateModelOperation CreateModelOperation()
+    {
+        return new StateModelOperation();
+    }
+
+    Task AddAsync(int id, int productId, int productQuantity);
+
+    Task<IStateModel> GetAsync(int id);
+
+    Task UpdateAsync(int id, int productId, int productQuantity);
+
+    Task DeleteAsync(int id);
+
+    Task<Dictionary<int, IStateModel>> GetAllAsync();
+
+    Task<int> GetCountAsync();
+}
